40745eba5f863c933af2640be829f7cf8a3c4644
test <test@asd.pl>
2022-10-18 09:59:16 +0200

    code logic

--- app/src/main/java/com/example/aplikacjalach/MainActivity.kt
+++ app/src/main/java/com/example/aplikacjalach/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.aplikacjalach
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.button)
+        val changeButton = findViewById<Button>(R.id.button2)
+
+        val input = findViewById<TextInputEditText>(R.id.inputText)
+        val output = findViewById<TextView>(R.id.textView)
+
+        addButton.setOnClickListener {
+            output.text = output.text.toString() + input.text.toString()
+        }
+
+        changeButton.setOnClickListener {
+            output.text = "Kamil Lach"
+        }
     }
 }
